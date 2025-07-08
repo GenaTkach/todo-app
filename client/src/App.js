27730c7e2f91d416fc83ca358b2715924ead060c
GenaTkach <genat@super-pharm.co.il>
2025-07-08 16:10:05 +0300

    Add DynamicBackground component with gradient animation and integrate into App

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./style/App.css";
 import React from "react";
 import TodoList from "./component/TodoList";
+import DynamicBackground from "./component/DynamicBackground";
 
 function App() {
   return (
     <div className="App">
+      <DynamicBackground />
       <TodoList />
     </div>
   );
---
+++ client/src/component/DynamicBackground.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style/DynamicBackground.css";
+
+const DynamicBackground = () => {
+  return <div className="dynamic-background"></div>;
+};
+
+export default DynamicBackground;
